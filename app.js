99e828ca5a4b38dc9070f1984b89af42ee4e1d0b
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-13 08:35:19 +0100

    Worked

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 const Class = (cl, id) => document.getElementsByClassName(cl)[id]
 const Id = id => document.getElementById(id)
+
+const SvG = (input) => {
+    let result = ""
+
+    for (let i = 0; i < input.length; i++) {
+        if (i > 0 && input[i - 1] === "<") result += input[i].toUpperCase()
+        else if (i > 1 && input[i - 1] === "-") result += input[i].toUpperCase()
+        else {
+            if (input[i] !== "-") result += input[i]
+
+        }
+    }
+
+    return result
+}
+
+
+Class("func", 0).addEventListener("click", () => {
+    Class("result", 0).value = SvG(Class("enter", 0).value)
+})
